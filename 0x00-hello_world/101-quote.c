09919e09996c4fbe7d374b8e46b785a0fde83ad9
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 02:14:22 +0000

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ *
+ * Description - uses printf and puts together
+ *
+ * Return: 1 if success, non-one if failure
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
