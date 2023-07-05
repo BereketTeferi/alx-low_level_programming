13599029f0a517557b3b483300e1e4cadf43de78
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 03:24:43 +0000

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Description - prints numbers fron one to nine
+ *
+ * Return: 0 if success non-zero if failure
+ */
+
+int main(void)
+{
+	for (int i=0; i<10; i++)
+	{
+		printf("%d",i);
+	}
+	printf("\n");
+
+	return (0);
+}
