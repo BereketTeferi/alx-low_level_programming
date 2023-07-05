011a36474650911b45c0b76f6050e5cd2a5d634c
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 01:01:53 +0000

    Added updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**Thia ia the main function for printig
+ * programming is like buio;ding a multilingual puzzle
+ * using puts avoiding printf
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
