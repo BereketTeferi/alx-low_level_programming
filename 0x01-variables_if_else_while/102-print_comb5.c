e757578cc88476c54abfba3e64a6f3b22a553211
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 04:56:35 +0000

    Added 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,22 +13,26 @@ int main(void)
 	int i,j,k,l;
 	for(i=48; i<58; i++)
 	{
-		if(l>48 && l<57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
 		for(j=48; j<58; j++)
 		{
-			putchar(i);
-			putchar(j);
-			putchar(' ');
 			for(k=48; k<58; k++)
 			{
 				for(l=48; l<58; l++)
 				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
 					putchar(k);
 					putchar(l);
+					if(i==57 && j==57 && k==57 && l==57)
+					{
+
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
