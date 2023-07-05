bcb2ccca9c8886049806a3f4fc1a473f5d151759
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 04:12:22 +0000

    Added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers combination
+ *
+ * Description - prints numbers combination
+ *
+ * Return: 0 if success non-zero if failure
+ */
+
+int main(void)
+{
+	for(int i=48; i<58; i++)
+	{
+		for(int j=i+1; j<58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if(i<56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	
+	return (0);
+}
+
