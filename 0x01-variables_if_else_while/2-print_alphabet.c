695a2c995de7b86707b464cd7386650edd5d4393
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 03:12:09 +0000

    Added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -display alphabet
+ *
+ * Description - display the alphabet a-z all in lower case
+ *
+ * Return: 0 if success non zero otherwise
+ */
+
+int main(void)
+{
+	char letter;
+
+	for(letter='a'; letter<='z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
