7b5a6ca896aad4841192bdb4c658e7746b97077c
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 04:02:10 +0000

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - displays numbers
+ *
+ * Description - display number 1-9 in ascending order sparated by comma and space
+ *
+ * Return: 0 if success non-zero if failure
+ */
+
+int main(void)
+{
+	for(int i=48; i<58; i++)
+	{
+		putchar(i);
+		if(i<57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
