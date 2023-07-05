16e1ae306bca4d63aab6b63df6c00bc6fcf2fcbc
BereketTeferi <bereketteferi87@gmail.com>
2023-07-05 01:31:21 +0000

    Added 5-printf,c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints using printf
+ *
+ * Description - Prints with proper grammar, but the outcomen...
+ *
+ * Return: 0 on success, non zero on failure
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
