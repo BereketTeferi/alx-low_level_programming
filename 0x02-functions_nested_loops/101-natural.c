08b9cc8502ddb043421249bf3b1b5d03a68ab2f3
Bereket <bereket@Ubuntu.myguest.virtualbox.org>
2023-07-19 01:01:02 +0300

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - compute all natural numbers multiple of 3 and 5
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int sum = 0, count;
+
+	for(count = 1; count <= 1024; count++)
+	{
+		if(count % 3 == 0)
+		{
+			sum += count;
+		}
+		else if(count % 5 == 0)
+		{
+			sum += count;
+		}
+	}
+
+	return (0);
+}
