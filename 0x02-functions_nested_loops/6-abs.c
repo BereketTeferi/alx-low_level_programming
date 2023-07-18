2893286a88eb4dc9c5d5e63e745ff89283edfe68
Bereket <bereket@Ubuntu.myguest.virtualbox.org>
2023-07-18 21:18:09 +0300

    Added abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - make absolute value
+ *
+ * @n: evaluated by absolute value
+ *
+ * Return: always 0 if success
+ */
+
+int _abs(int n)
+{
+	_putchar(abs(n));
+
+	return (0);
+}
