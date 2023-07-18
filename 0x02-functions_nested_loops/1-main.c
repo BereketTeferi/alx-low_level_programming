400f08ea079b3a22fe3dc02ceaca1dc0129e3afa
Bereket <bereket@Ubuntu.myguest.virtualbox.org>
2023-07-18 18:46:50 +0300

    Added alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -7,6 +7,16 @@ void print_alphabet(void);
  * Return: Always 0.
  */
 
+void print_alphabet(void)
+{
+	int i;
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n);
+}
+
 int main(void)
 {
     print_alphabet();
