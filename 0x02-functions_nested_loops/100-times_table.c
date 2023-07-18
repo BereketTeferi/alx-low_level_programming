6dcaa5c62dd5c95f73210a0aeda57eca2700521a
Bereket <bereket@Ubuntu.myguest.virtualbox.org>
2023-07-19 00:32:06 +0300

    Added 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,64 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table for number inputed
+ * print_times_table_row - prints one row of the times table
+ *
+ * @i: first number in the row
+ * @j: second number in the row
+ * @n: maximum number in the times table
+ */
+
+void print_times_table_row(int i, int j)
+{
+	int k;
+
+	if (j != 0)
+	{
+		_putchar(44);
+		_putchar(32);
+		_putchar(32);
+		_putchar(32);
+	}
+	k = i * j;
+	if (k > 99)
+	{
+		int l = k % 10;
+		int o = k / 100;
+		int m = (k % 100) / 10;
+		_putchar(o + '0');
+		_putchar(m + '0');
+		_putchar(l + '0');
+	}
+	else if (k > 9 && k < 100)
+	{
+		int l = k % 10;
+		int m = (k - l) / 10;
+		_putchar(m + '0');
+		_putchar(l + '0');
+	}
+	else
+	{
+		_putchar(k + '0');
+	}
+}
+
+/**
+ * print_times_table - prints the times table for number inputed
  *
  * @n: number passed to function
  */
 
 void print_times_table(int n)
 {
-	int i, j, k, l, m, o;
-	
+	int i, j;
+
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
-				if (k > 99)
-				{
-					l = k % 10;
-					o = k / 100;
-					m = (k % 100) / 10;
-					_putchar(44);
-					_putchar(32);
-					_putchar(o + '0');
-					_putchar(m + '0');
-					_putchar(l + '0');
-				}
-				else if (k > 9 && k < 100)
-				{
-					l = k % 10;
-					m = (k - l) / 10;
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-					_putchar(m + '0');
-					_putchar(l + '0');
-				}
-				else
-				{
-					if (j != 0)
-					{
-						_putchar(44);
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(k + '0');
-				}
+				print_times_table_row(i, j);
 			}
 			_putchar('\n');
 		}
