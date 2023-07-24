d0b028f50033fe3c26e40aade90d0cd3b38ff54d
BereketTeferi <bereketteferi87@gmail.com>
2023-07-24 12:41:19 -0700

    Added strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - clone of strcpy
+ *
+ * @dest: destination of string
+ * @src: source of string
+ *
+ * Return: destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
