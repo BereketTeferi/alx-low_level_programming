581b8b5c4c6e86d5a9ef10052a662b9368ca971b
BereketTeferi <bereketteferi87@gmail.com>
2023-07-26 16:06:59 +0000

    Added rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,21 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp, i;
+	int temp, i, temp2;
 
-	for (i = 0; i <= n / 2; i++)
+	if (n == 2)
 	{
-		temp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = temp;
+		temp2 = a[0];
+		a[0] = a[1];
+		a[1] = temp2;
+	}
+	else
+	{
+		for (i = 0; i <= n / 2; i++)
+		{
+			temp = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = temp;
+		}
 	}
 }
