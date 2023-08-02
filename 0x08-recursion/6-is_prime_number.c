fbbc3b0b34784be910900a259938745f02e1985c
BereketTeferi <bereketteferi87@gmail.com>
2023-08-02 19:27:56 +0000

    Added is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,42 @@
 #include "main.h"
+int prime_tester(int n, int x);
 
 /**
- * is_prime_number - find if prime number recurcively
+ * is_prime_number - checks if an input is prime
  *
- * @n: number passed
+ * @n: input
  *
- * Return: 1 if prime 0 otherwise
+ * Return: 1 if prime 0 if not
  */
 
 int is_prime_number(int n)
 {
-	if (n % is_prime_number(n - 1) == 0)
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime_tester(n, n - 1));
+}
+
+/**
+ * prime_tester - tests if a number is prime
+ * by finding the modulo of all the numbers below it
+ *
+ * @n: the number to determine
+ * @x: the initial value
+ *
+ * Return: 1 if n is prime otherwise 0
+ */
+
+int prime_tester(int n, int x)
+{
+	if (x == 1)
 	{
 		return (1);
 	}
-	return (0);
+	else if (n % x == 0 && x < n && x != 1)
+	{
+		return (0);
+	}
+	return (prime_tester(n, x - 1));
 }
