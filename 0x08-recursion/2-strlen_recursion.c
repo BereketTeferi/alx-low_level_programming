457be56fa7da7fc60a4ed39ce4bfbb4f6cb90c76
BereketTeferi <bereketteferi87@gmail.com>
2023-08-02 10:38:12 +0000

    Added strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - prints length of string
+ *
+ * @s: string passed to function
+ *
+ * Return: size of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
