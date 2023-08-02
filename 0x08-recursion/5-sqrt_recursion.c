f025ff976458e43b90d5cf809e99e5206560b9df
BereketTeferi <bereketteferi87@gmail.com>
2023-08-02 19:13:38 +0000

    Added sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,55 +1,53 @@
 #include "main.h"
-long int sqrt_helper(int n, int min, int max);
 
 /**
- * _sqrt_recursion - square root of a number recursively
+ * _sqrt_recursion2 - fills memory with a constant byte.
  *
- * @n: number to be rooted
+ * @n: first bytes of the memory
+ * @sqrt: first bytes of the memory
+ * @res: first bytes of the memory
+ * Return: -
+ */
+
+float _sqrt_recursion2(int n, float sqrt, float res)
+{
+	if (sqrt == res)
+	{
+		if ((int)sqrt == sqrt)
+		{
+			return (sqrt);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	res = sqrt;
+	sqrt = (n / res + res) / 2;
+	return (_sqrt_recursion2(n, sqrt, res));
+}
+
+/**
+ * _sqrt_recursion - fills memory with a constant byte.
  *
- * Return: square root of the number
+ * @n: first bytes of the memory
+ *
+ * Return: square root of n
  */
 
 int _sqrt_recursion(int n)
 {
-	if (sqrt_helper(n, 0, n) == (int)sqrt_helper(n, 0, n))
+	float res, sqrt;
+
+	sqrt = n / 2;
+	res = 0;
+	if (n == 1)
 	{
-		return (sqrt_helper(n, 0, n));
+		return (1);
 	}
-	else
+	if (n < 0)
 	{
 		return (-1);
 	}
-}
-
-/**
- * sqrt_helper - helper function
- *
- * @n: number  to be square rooted
- * @min: minimun number guessed
- * @max: maximum number guessed
- *
- * Return: square root
- */
-
-long int sqrt_helper(int n, int min, int max)
-{
-	int mid = (min + max) / 2;
-	int sq = mid * mid;
-
-	if (min > max)
-	{
-		return (-1);
-	}
-	if (sq == n)
-	{
-		return (mid);
-	}
-	else if (sq < n)
-	{
-		return (sqrt_helper(n, mid + 1, max));
-	}
-	else
-	{
-		return (sqrt_helper(n, min, mid - 1));
-	}
+	return (_sqrt_recursion2(n, sqrt, res));
 }
