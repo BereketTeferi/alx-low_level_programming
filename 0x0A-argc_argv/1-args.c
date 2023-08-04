ea26043fe025d17910b3da2daaa18c81a4ae0be4
BereketTeferi <bereketteferi87@gmail.com>
2023-08-04 19:27:38 +0000

    Added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number of command-line arguments
+ * @argv: argument passed to program
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	(void)argc;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
