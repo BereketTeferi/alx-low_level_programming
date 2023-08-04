6e1a6badd234ac1369b48d2de32158d016c878f8
BereketTeferi <bereketteferi87@gmail.com>
2023-08-04 19:55:14 +0000

    Added  3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -59,11 +59,12 @@ int main(int argc, char *argv[])
 	if (argc < 3)
 	{
 		printf("%s\n", "Empty");
+		return (1);
 	}
 	else
 	{
 		num = _atoi(argv[1]) * _atoi(argv[2]);
+		printf("%d\n", num);
+		return (0);
 	}
-	printf("%d\n", num);
-	return (0);
 }
