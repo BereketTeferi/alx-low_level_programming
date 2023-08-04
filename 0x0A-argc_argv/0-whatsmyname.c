efd75f85a91f72561b4c73726c6fafcf32e32cfc
BereketTeferi <bereketteferi87@gmail.com>
2023-08-04 19:21:05 +0000

    Added 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * main - main function of c
  *
  * @argc: number of command-line arguments
- * @argv[]: argument passed to program
+ * @argv: argument passed to program
  *
  * Return: 0
  */
 
-int main(int argc, char** argv[])
+int main(int argc, char **argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
