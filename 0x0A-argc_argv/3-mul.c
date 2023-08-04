fce93272c5198535c445fd191db1741c9c2a2bca
BereketTeferi <bereketteferi87@gmail.com>
2023-08-04 19:59:05 +0000

    Added 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,46 +1,29 @@
 #include "main.h"
 
 /**
- * _atoi - extract int from string
+ * atoi - extract int from string
  *
  * @s: string passed
  *
  * Return: 0 if no numbers in the string
  */
 
-int _atoi(char *s)
+int atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int num = 0;
+	int sign = 1;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
+	if (*s && *s == '-')
 	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		sign = -1;
+		s++;
 	}
-	if (f == 0)
-		return (0);
-	return (n);
+	while (*s && *s >= '0' && *s <= '9')
+	{
+		num = num * 10 + (*s - '0');
+		s++;
+	}
+	return (num * sign);
 }
 
 /**
@@ -54,7 +37,7 @@ int _atoi(char *s)
 
 int main(int argc, char *argv[])
 {
-	int num;
+	int num1, num2;
 
 	if (argc < 3)
 	{
@@ -63,8 +46,9 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		num = _atoi(argv[1]) * _atoi(argv[2]);
-		printf("%d\n", num);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
 		return (0);
 	}
 }
