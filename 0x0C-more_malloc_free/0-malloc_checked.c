241bcdb7648159179f892d3f67664cddd0dce6b5
BereketTeferi <bereketteferi87@gmail.com>
2023-08-10 10:36:24 -0700

    Added 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: size of memory passed
+ *
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	if (b == 0)
+	{
+		exit(98);
+	}
+	if (b == INT_MAX)
+	{
+		exit(98);
+	}
+	return (malloc(sizeof(b)));
+}
