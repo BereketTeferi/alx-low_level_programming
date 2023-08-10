7092c38408630310a1f2dddfe29b7716f9b52f65
BereketTeferi <bereketteferi87@gmail.com>
2023-08-10 12:59:12 -0700

    Added 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,7 +11,7 @@
 
 int *array_range(int min, int max)
 {
-	int i, j;
+	int i, j, k = 0;
 	int *arr = NULL;
 
 	if (min > max)
@@ -21,6 +21,9 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
-		arr[i] = i;
+	{
+		arr[k] = i;
+		k++;
+	}
 	return (arr);
 }
