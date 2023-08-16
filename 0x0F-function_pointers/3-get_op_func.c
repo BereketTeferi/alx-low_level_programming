4003aa49e330c7a382830bcd45b52afed082d8d9
BereketTeferi <bereketteferi87@gmail.com>
2023-08-16 09:22:33 -0700

    Added files for task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,11 +21,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 5; i++)
+	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (strcmp(s, ops[i].op))
 			return (ops[i].f);
 	}
 	return (NULL);
