4ea945a1ef61ab8bb6557a2bb6fb8bfb48418fa7
BereketTeferi <bereketteferi87@gmail.com>
2023-08-16 09:09:18 -0700

    Added files for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - checks the program
+ *
+ * @arcou: argument counter
+ * @arr: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int arcou, char **arr)
+{
+	int (*op)(int, int);
+
+	if (arcou != 4)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	op = get_op_func(arr[2]);
+	if (!op)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	printf("%d\n", op(atoi(arr[1]), atoi(arr[3])));
+	return (0);
+}
