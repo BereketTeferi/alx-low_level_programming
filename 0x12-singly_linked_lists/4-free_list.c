534461762b10d573130e5d590093b3d92094ba44
BereketTeferi <bereketteferi87@gmail.com>
2023-08-24 13:21:51 +0000

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,27 +3,6 @@
 #include <string.h>
 
 /**
- * list_len - counts number of elements in
- * linked list
- *
- * @h: head of the linked list
- *
- * Return: number of elements in linked list
- */
-
-size_t list_len(const list_t *h)
-{
-	int count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
  * free_list - frees linked list
  *
  * @head: head of the linked list
@@ -31,19 +10,15 @@ size_t list_len(const list_t *h)
 
 void free_list(list_t *head)
 {
-	int length = list_len(head), i;
 	list_t *list;
 
-	for (i = 0; i <= length; i++)
+	if (head == NULL)
+		return;
+	while (head)
 	{
-		while (head != NULL)
-		{
-			list = head;
-			head = head->next;
-			free(list->str);
-			free(list);
-		}
+		list = head->next;
+		free(head->str);
+		free(head);
+		head = list;
 	}
-	free(head->str);
-	free(head);
 }
