129e33da44b774b679a7e776b9de4e5f8b2159a1
BereketTeferi <bereketteferi87@gmail.com>
2023-08-29 14:22:10 +0000

    Fixed 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,14 +14,14 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *tmp;
 
 	if (!head)
-		return (nodes);
+		return (0);
 	tmp = (listint_t *)head;
 	while (tmp)
 	{
 		printf("[%p] %d\n", (void *)tmp, tmp->n);
 		nodes++;
 
-		if (tmp->next >= tmp)
+		if (tmp->next && tmp > tmp->next)
 		{
 			printf("-> [0x%p] %d\n", (void *)tmp->next, tmp->next->n);
 			exit(98);
