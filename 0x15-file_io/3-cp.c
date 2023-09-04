c106c63eb0a85da7b3f298ae52fdb150f853ead2
BereketTeferi <bereketteferi87@gmail.com>
2023-09-04 12:53:42 +0000

    Added 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdarg.h>
-
-#define BUFFER_SIZE 1024
+#include "main.h"
 
 /**
- *  * print_error - Print an error message and exit with the given code.
- *   * @code: The exit code.
- *    * @format: The format string for the error message.
- *     */
-void print_error(int code, const char *format, ...)
+ * print_error - Print an error message and exit.
+ *
+ * @file_from: File descriptor for source file.
+ * @file_to: File descriptor for destination file.
+ * @filename: Name of the file.
+ *
+ * Return: 98 if can't read
+ * 99 if can't write
+ * 0 otherwise.
+ */
+
+int print_error(int file_from, int file_to, char *filename)
 {
-	    va_list args;
-	        va_start(args, format);
-		    dprintf(STDERR_FILENO, format, args);
-		        va_end(args);
-			    exit(code);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (0);
 }
+
+/**
+ * close_all - Close file descriptors.
+ *
+ * @file_from: File descriptor for source file.
+ * @file_to: File descriptor for destination file.
+ *
+ * Return: 0 on success
+ * 100 on error.
+ */
+
+int close_all(int file_from, int file_to)
+{
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return 0;
+}
+
+/**
+ * main - Copy the content of one file to another.
+ *
+ * @argc: The number of arguments.
+ * @argv: An array of strings.
+ *
+ * Return: 97 if the number of arguments is not correct
+ * 98 if file_from does not exist or cannot be read
+ * 99 if writing to file_to fails
+ * 100 if closing a file descriptor fails
+ * 0 on success.
+ */
 
 int main(int argc, char **argv)
 {
-	    int file_from, file_to;
-	        ssize_t read_file_from, write_file_to;
-		    char buffer[BUFFER_SIZE];
+	int file_from, file_to;
+	ssize_t read_file_from, write_file_to;
+	char buffer[1024];
 
-		        if (argc != 3)
-				    {
-					            print_error(97, "Usage: cp file_from file_to\n");
-						        }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-			    file_from = open(argv[1], O_RDONLY);
-			        if (file_from == -1)
-					    {
-						            print_error(98, "Error: Can't read from file %s\n", argv[1]);
-							        }
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-				    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-				        if (file_to == -1)
-						    {
-							            print_error(99, "Error: Can't write to %s\n", argv[2]);
-								        }
+	if (file_from == -1 || file_to == -1)
+	{
+		print_error(file_from, file_to, (file_from == -1) ? argv[1] : argv[2]);
+	}
+	while ((read_file_from = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		write_file_to = write(file_to, buffer, read_file_from);
+		if (write_file_to == -1 || read_file_from != write_file_to)
+		{
+			print_error(0, -1, argv[2]);
+		}
+	}
+	close_all(file_from, file_to);
 
-					    while ((read_file_from = read(file_from, buffer, sizeof(buffer))) > 0)
-						        {
-								        write_file_to = write(file_to, buffer, read_file_from);
-									        if (write_file_to == -1 || read_file_from != write_file_to)
-											        {
-													            print_error(99, "Error: Can't write to %s\n", argv[2]);
-														            }
-										    }
-
-					        if (read_file_from == -1)
-							    {
-								            print_error(98, "Error: Can't read from file %s\n", argv[1]);
-									        }
-
-						    if (close(file_from) == -1 || close(file_to) == -1)
-							        {
-									        print_error(100, "Error: Can't close fd %d\n", (close(file_from) == -1) ? file_from : file_to);
-										    }
-
-						        return 0;
+	return 0;
 }
-
