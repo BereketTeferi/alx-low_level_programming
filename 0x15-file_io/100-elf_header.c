a44ea860b7d3be5121cfdfbfb12d3f193dea8eb8
BereketTeferi <bereketteferi87@gmail.com>
2023-09-07 10:45:31 +0000

    Added 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,9 +15,9 @@ int main(int argc, char **argv)
 	char st[64];
 	int fp, rf;
 
-	if (agrc != 2)
+	if (argc != 2)
 	{
-		dprinf(STDERR_FILENO, "Usage: %s elf_file\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s elf_file\n", argv[0]);
 		exit(98);
 	}
 
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: can't open this file %s\n", argv[1]);
 		exit(98);
 	}
-	rf = read(file, st, 64);
+	rf = read(fp, st, 64);
 	if (rf == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from this file %s\n", argv[1]);
